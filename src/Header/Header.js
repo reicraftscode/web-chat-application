26085aa746f02aef6582a29323ce7861a5347c7c
c1623793 <c1623793CULM@gmail.com>
2020-11-10 05:30:05 +0800

    feat:added separate header

---
+++ src/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./Header.module.scss";
+const Header = () => {
+	return (
+		<div>
+			{/* Logo wrapper div */}
+			<div className={`bg-dark ${style.logoWrapper}`}>
+				<div className={`${style.logoIconTextWrapper}`}>
+					<img
+						className={style.beeIcon}
+						alt="bee icon"
+						src="./assets/photos/bee.svg"
+					></img>
+					<h3 className={style.beeText}>ChatBee</h3>
+				</div>
+			</div>
+			{/* End logo wrapper div */}
+		</div>
+	);
+};
+export default Header;
