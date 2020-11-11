2a405f58db6daab1b0e0be364eabdc98ab74202e
c1623793 <c1623793CULM@gmail.com>
2020-11-12 01:39:05 +0800

    added some routing

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import "./custom.scss";
 import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import Exit from "./Exit/Exit";
 
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+const Routing = () => {
+	return (
+		<Router>
+			<Switch>
+				{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+				<Route exact path="/" component={App} />
+				<Route exact path="/exit" component={Exit} />
+			</Switch>
+		</Router>
+	);
+};
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Routing />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
