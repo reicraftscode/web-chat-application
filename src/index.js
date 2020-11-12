98e7d1be01b756843357a2d247a57f1264297f38
c1623793 <c1623793CULM@gmail.com>
2020-11-13 01:14:50 +0800

    feat:added new route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import Exit from "./Exit/Exit";
+import Chat from "./Chats/Chats";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 const Routing = () => {
@@ -14,6 +15,7 @@ const Routing = () => {
 				{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
 				<Route exact path="/" component={App} />
+				<Route exact path="/chat" component={Chat} />
 				<Route exact path="/exit" component={Exit} />
 			</Switch>
 		</Router>
