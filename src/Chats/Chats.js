5acea239aa202ef4f1c6dbebf3b958e10ffdecbf
c1623793 <c1623793CULM@gmail.com>
2020-11-13 05:29:56 +0800

    updated the chats component

--- src/Chats/Chats.js
+++ src/Chats/Chats.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../Header/Header";
+import io from "socket.io-client";
+const API_LINK = "http://localhost:3000";
+const socket = io.connect(API_LINK, { reconnection: true });
+
 const Chats = () => {
+	useEffect(() => {
+		socket.on("connect", () => {
+			socket.on("broadcast", (message) => {
+				console.log(message);
+			});
+			socket.emit("account", { username: "haxxor69" });
+		});
+		socket.on("disconnect", () => {
+			socket.on("broadcast", (message) => {
+				console.log(message);
+			});
+		});
+	}, []);
 	return (
 		<div>
 			<Header />
