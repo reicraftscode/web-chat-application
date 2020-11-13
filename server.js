ba042e3e978cb5fce6429bf17371feb7bc38518f
c1623793 <c1623793CULM@gmail.com>
2020-11-13 11:31:25 +0800

    feat:added socket logic

--- server.js
+++ server.js
@@ -1,21 +1,59 @@
-const app = require("express")();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const { Socket } = require("socket.io");
 
-io.on("connection", (socket) => {
-	console.log(`Connected: ${socket.id}`);
-	socket.on("disconnect", () => console.log(`Disconnected: ${socket.id}`));
-	socket.on("join", (room) => {
-		console.log(`Socket ${socket.id} joining ${room}`);
-		socket.join(room);
-	});
-	socket.on("chat", (data) => {
-		const { message, room } = data;
-		console.log(`msg: ${message}, room: ${room}`);
-		io.to(room).emit("chat", message);
-	});
-});
+const server = require("http").createServer();
+const options = {
+	cors: true,
+	origins: ["http://localhost:3001"],
+};
+const io = require("socket.io")(server, options);
 
-http.listen(3000, () => {
-	console.log("listening on *:3000");
-});
+// io.on("connection", (socket) => {
+// 	console.log(`Connected: ${socket.id}`);
+// 	socket.on("disconnect", () => console.log(`Disconnected: ${socket.id}`));
+// 	socket.on("join", (room) => {
+// 		console.log(`Socket ${socket.id} joining ${room}`);
+// 		socket.join(room);
+// 	});
+// 	socket.on("chat", (data) => {
+// 		const { message, room } = data;
+// 		console.log(`msg: ${message}, room: ${room}`);
+// 		io.to(room).emit("chat", message);
+// 	});
+// });
+try {
+	const users = {};
+	io.on("connection", (client) => {
+		client.on("username", (username) => {
+			console.log(username + " has logged in");
+			client.broadcast.emit("broadcast", `${username} has connected`);
+			const user = {
+				name: username,
+				id: client.id,
+			};
+			users[client.id] = user;
+			io.emit("connected", user);
+			io.emit("users", Object.values(users));
+		});
+
+		client.on("send", (message) => {
+			io.emit("message", {
+				text: message,
+				date: new Date().toISOString(),
+				user: users[client.id],
+			});
+		});
+
+		client.on("disconnect", () => {
+			const username = users[client.id];
+			if (username != undefined) {
+				console.log(username.name + " has logged out");
+				client.broadcast.emit("broadcast", `${username.name} has disconnected`);
+			}
+			delete users[client.id];
+			io.emit("disconnected", client.id);
+		});
+	});
+} catch (err) {
+	console.error(err);
+}
+server.listen(3000);
