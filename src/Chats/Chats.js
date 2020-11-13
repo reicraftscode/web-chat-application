e48ee9fccade84bb9dbf5cb34ad77d2306e35ca2
c1623793 <c1623793CULM@gmail.com>
2020-11-13 11:32:23 +0800

    feat:prepared component for sockets

--- src/Chats/Chats.js
+++ src/Chats/Chats.js
@@ -1,23 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import io from "socket.io-client";
+import moment from "moment";
 const API_LINK = "http://localhost:3000";
-const socket = io.connect(API_LINK, { reconnection: true });
+const socketOptions = {
+	reconnection: true,
+	transports: ["websocket", "polling"],
+};
+const socket = io.connect(API_LINK, socketOptions);
+const username = prompt("username");
 
 const Chats = () => {
+	const [users, setUsers] = useState([]);
+	const [message, setMessage] = useState("");
+	const [messages, setMessages] = useState([]);
 	useEffect(() => {
 		socket.on("connect", () => {
-			socket.on("broadcast", (message) => {
-				console.log(message);
-			});
-			socket.emit("account", { username: "haxxor69" });
+			socket.emit("username", username);
 		});
-		socket.on("disconnect", () => {
-			socket.on("broadcast", (message) => {
-				console.log(message);
+		socket.on("users", (users) => {
+			setUsers(users);
+		});
+		socket.on("message", (message) => {
+			setMessages((messages) => [...messages, message]);
+		});
+		socket.on("connected", (user) => {
+			setUsers((users) => [...users, user]);
+		});
+		socket.on("disconnected", (id) => {
+			setUsers((users) => {
+				return users.filter((user) => user.id !== id);
 			});
 		});
+		socket.on("broadcast", (message) => {
+			console.log(message);
+		});
+		// return () => socket.disconnect();
 	}, []);
+
+	const submit = (event) => {
+		event.preventDefault();
+		socket.emit("send", message);
+		setMessage("");
+	};
+
 	return (
 		<div>
 			<Header />
