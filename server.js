a0ef5da6081d2206e6a50d3493eb93620b4752d1
c1623793 <c1623793CULM@gmail.com>
2020-11-16 21:06:45 +0800

    feat:made server connection and disconnection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { createSocket } = require("dgram");
 const express = require("express");
 const PORT = 3000;
 const app = require("express")();
@@ -10,8 +11,14 @@ const io = require("socket.io")(http, {
 		// credentials: true,
 	},
 	transports: ["websocket", "polling"],
+	pingInterval: 1000,
+	pingTimeout: 2000,
 });
 let loggedUsers = [];
+
+const getUniqueListBy = (arr, key) => {
+	return [...new Map(arr.map((item) => [item[key], item])).values()];
+};
 
 const STATIC_CHANNELS = ["global_notifications", "global_chat"];
 io.on("connection", (socket) => {
@@ -19,13 +26,14 @@ io.on("connection", (socket) => {
 	console.log("new client connected");
 	//listen to disconnected clients
 	socket.on("disconnect", () => {
-		console.log("client disconnected");
-		loggedUsers = loggedUsers.filter((data) => {
-			if (data.socketid != socket.id) {
-				return data;
-			}
-		});
-		socket.emit("onlineusers", loggedUsers);
+		console.log("client disconnected:" + socket.id);
+		const index = loggedUsers.findIndex((x) => x.socketid === socket.id);
+		if (index !== undefined) loggedUsers.splice(index, 1);
+		console.log("after" + loggedUsers);
+		io.emit("removeusers", loggedUsers);
+	});
+	socket.on("logout", (data) => {
+		console.log("user " + data + " has logout");
 	});
 	//emit a message once a user connected
 	socket.on("login", (data) => {
