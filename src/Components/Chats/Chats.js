15a6ee8648b2f0c9b457615b1d95378beac25d11
c1623793 <c1623793CULM@gmail.com>
2020-11-16 21:06:52 +0800

    feat:made server connection and disconnection

--- src/Components/Chats/Chats.js
+++ src/Components/Chats/Chats.js
@@ -4,41 +4,42 @@ import { io, socket } from "../../Service/Socket";
 
 const Chats = ({ username }) => {
 	const [userList, setUserList] = useState([]);
+
 	const socketRef = useRef();
 	const id = Math.floor(Math.random() * 5);
 	useEffect(() => {
 		socketRef.current = socket;
+
 		socketRef.current.on("connect", () => {
 			const data = {
 				socketid: socketRef.current.id,
 				id: id,
 				username: username,
-				time: moment().format("MMMM DD,YYYY hh:mm A"),
+				// time: moment().format("MMMM DD,YYYY hh:mm A"),
 			};
 			socketRef.current.emit("login", data);
 		});
 		socketRef.current.on("onlineusers", (data) => {
 			setUserListToState(data);
 		});
-		socketRef.current.on("login", (data) => {});
-
-		socketRef.current.on("disconnect", () => {
-			const data = { id: id };
-			socketRef.current.emit("logout", data);
+		socketRef.current.on("removeusers", (data) => {
+			setUserListToState(data);
 		});
-	}, [id, username, userList]);
+		socketRef.current.on("disconnect", (data) => {
+			socketRef.current.emit("logout", { username: username });
+		});
+	}, [id, username]);
 	const setUserListToState = (data) => {
 		setUserList(data);
 	};
 	return (
-		<div>
-			<div>
+		<div className="row border border-info">
+			<div className="col col-md-2">
 				{userList.map((user, index) => {
 					return (
 						<ul key={index}>
 							<p>
 								{username === user.username ? "You" : user.username} logged in
-								on {user.time}
 							</p>
 						</ul>
 					);
