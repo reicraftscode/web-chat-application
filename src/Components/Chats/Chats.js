4e33e24476a13468f4f3d36945a329153b9f4d8a
c1623793 <c1623793CULM@gmail.com>
2020-11-17 17:56:51 +0800

    fix:keeping track of users

--- src/Components/Chats/Chats.js
+++ src/Components/Chats/Chats.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import moment from "moment";
 import { io, socket } from "../../Service/Socket";
+import Messages from "../Messages/Messages";
 
 const Chats = ({ username }) => {
 	const [userList, setUserList] = useState([]);
@@ -17,33 +18,46 @@ const Chats = ({ username }) => {
 				username: username,
 				// time: moment().format("MMMM DD,YYYY hh:mm A"),
 			};
+			socketRef.current.emit("message", {
+				data: { message: `${username} has logged in` },
+			});
 			socketRef.current.emit("login", data);
 		});
 		socketRef.current.on("onlineusers", (data) => {
-			setUserListToState(data);
+			if (!userList.includes(data)) {
+				setUserListToState(data);
+			}
 		});
 		socketRef.current.on("removeusers", (data) => {
-			setUserListToState(data);
+			if (!userList.includes(data)) {
+				setUserListToState(data);
+			}
 		});
 		socketRef.current.on("disconnect", (data) => {
 			socketRef.current.emit("logout", { username: username });
 		});
-	}, [id, username]);
+	}, [id, username, userList]);
 	const setUserListToState = (data) => {
 		setUserList(data);
 	};
 	return (
-		<div className="row border border-info">
-			<div className="col col-md-2">
-				{userList.map((user, index) => {
-					return (
-						<ul key={index}>
-							<p>
-								{username === user.username ? "You" : user.username} logged in
-							</p>
-						</ul>
-					);
-				})}
+		<div>
+			<div className="row">
+				<div className="col col-md-2">
+					<h2 className="ml-2">Users</h2>
+					{userList.map((user, index) => {
+						return (
+							<ul className="list-group" key={index}>
+								<li className="list-group-item">
+									{username === user.username ? "You" : user.username}
+								</li>
+							</ul>
+						);
+					})}
+				</div>
+				<div className="ml-5">
+					<Messages username={username} />
+				</div>
 			</div>
 		</div>
 	);
