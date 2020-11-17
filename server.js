301d740990dd6cac93d1c4285756604ab64882bc
c1623793 <c1623793CULM@gmail.com>
2020-11-17 17:35:11 +0800

    feat:comments+messages+refactored code

--- server.js
+++ server.js
@@ -16,30 +16,44 @@ const io = require("socket.io")(http, {
 });
 let loggedUsers = [];
 
-const getUniqueListBy = (arr, key) => {
-	return [...new Map(arr.map((item) => [item[key], item])).values()];
-};
-
 const STATIC_CHANNELS = ["global_notifications", "global_chat"];
 io.on("connection", (socket) => {
 	//prompt someone connected
 	console.log("new client connected");
 	//listen to disconnected clients
 	socket.on("disconnect", () => {
-		console.log("client disconnected:" + socket.id);
+		//check index
 		const index = loggedUsers.findIndex((x) => x.socketid === socket.id);
+		//remove if index detected
 		if (index !== undefined) loggedUsers.splice(index, 1);
-		console.log("after" + loggedUsers);
+		// console.log("removed" + JSON.stringify(loggedUsers));
+		//emit remove users to remove user in frontend
 		io.emit("removeusers", loggedUsers);
 	});
 	socket.on("logout", (data) => {
-		console.log("user " + data + " has logout");
+		//if socket is disconnected for some reason
+		console.log("user " + data.username + " has logout");
 	});
 	//emit a message once a user connected
 	socket.on("login", (data) => {
+		//inform about a user has logged in
 		console.log(data.username + " has logged in ");
-		loggedUsers.push(data);
+		//if data is not in array then push the data
+		if (!loggedUsers.includes(data)) {
+			loggedUsers.push(data);
+		}
+		//add to online users
 		io.emit("onlineusers", loggedUsers);
+		//emit a welcome message
+		io.emit("message", {
+			data: {
+				username: data.username,
+				message: `${data.username} has logged in `,
+			},
+		});
+	});
+	socket.on("message", (data) => {
+		console.log(data);
 	});
 });
 
