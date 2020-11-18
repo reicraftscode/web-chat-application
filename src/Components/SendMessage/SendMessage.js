bb864f7a6f7a91aad161276e857848e2bb073ad5
c1623793 <c1623793CULM@gmail.com>
2020-11-18 17:27:17 +0800

    fix:moved the lag when sending

--- src/Components/SendMessage/SendMessage.js
+++ src/Components/SendMessage/SendMessage.js
@@ -2,21 +2,43 @@ import React, { useRef, useEffect, useState } from "react";
 import { io, socket } from "../../Service/Socket";
 import moment from "moment";
 
-const SendMessage = () => {
+const SendMessage = ({ username }) => {
+	const [message, setMessage] = useState("");
 	const socketRef = useRef();
-	const [messages, setMessages] = useState([]);
-	useEffect(() => {
-		socketRef.current = socket;
-		socketRef.current.on("message", (data) => {
-			console.log(data);
-		});
-	}, []);
+	socketRef.current = socket;
+	const handleSendMessage = () => {
+		if (message === "") {
+			alert("Please enter a message");
+		} else {
+			socketRef.current.emit("message", {
+				data: {
+					username: username,
+					message: message,
+					time: moment().format("MMMM DD,YYYY hh:mm A"),
+				},
+			});
+			setMessage("");
+		}
+	};
+	const handleMessageText = (e) => {
+		setMessage(e.target.value);
+	};
+	const handleInputEnter = (e) => {
+		if (e.keyCode === 13) {
+			handleSendMessage();
+		}
+	};
 	return (
 		<div>
 			<div>
-				{messages.map((message) => {
-					return <p>{message}</p>;
-				})}
+				<input
+					type="text"
+					onChange={handleMessageText}
+					value={message}
+					required={true}
+					onKeyDown={handleInputEnter}
+				/>
+				<button onClick={() => handleSendMessage()}>Test emit</button>
 			</div>
 		</div>
 	);
