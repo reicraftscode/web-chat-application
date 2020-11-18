6f92e040eddd9a17ef1b481a616b7685d16743e4
c1623793 <c1623793CULM@gmail.com>
2020-11-19 06:43:18 +0800

    feat:added router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./custom.scss";
 import App from "./App/App";
+import Exit from "./Exit/Exit";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+const RoutedComponent = () => (
+	<Router>
+		<div>
+			<Route exact path="/" component={App} />
+			<Route exact path="/exit" component={Exit} />
+		</div>
+	</Router>
+);
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<RoutedComponent />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
