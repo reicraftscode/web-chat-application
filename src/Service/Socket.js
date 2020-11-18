a92025b4b658cbc87c1f83ffd480d84c26d11850
c1623793 <c1623793CULM@gmail.com>
2020-11-18 17:26:59 +0800

    fix:fixed some confusing syntax

--- src/Service/Socket.js
+++ src/Service/Socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 const API_LINK = "http://localhost:3000/";
-const socket = io.connect(API_LINK);
+const socket = io(API_LINK, { transports: ["websocket", "polling"] });
 
 export { socket, io };
