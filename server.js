1e94dcd55f8152316c57cec7c196b8ab81a31f02
c1623793 <c1623793CULM@gmail.com>
2020-11-19 16:38:47 +0800

    fix:server persists data

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const PORT = 3000;
-const app = require("express")();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const formidable = require("formidable");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/static", express.static("public"));
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
 	cors: {
@@ -12,6 +22,11 @@ const io = require("socket.io")(http, {
 	transports: ["websocket"],
 	timeout: 15000,
 });
+const { connection, testConnection } = require("./mongoose/config");
+let UserModel = require("./mongoose/schemas/User");
+let MessageModel = require("./mongoose/schemas/Message");
+testConnection();
+
 let loggedUsers = [];
 
 io.on("connection", (socket) => {
@@ -50,6 +65,106 @@ io.on("connection", (socket) => {
 	});
 	socket.on("message", (data) => {
 		io.emit("message", data);
+	});
+});
+app.post("/user/create", (req, res) => {
+	const form = formidable({
+		uploadDir: __dirname + "/public", // don't forget the __dirname here
+		keepExtensions: true,
+	});
+	form.parse(req, (err, fields, files) => {
+		if (err) console.log(err);
+		// console.log(fields);
+		// console.log(files);
+		if (files.file) {
+			const filename = files.file.path.split("/").pop();
+			const user = UserModel();
+			user.username = fields.username;
+			user.picture = "http://localhost:3000/static/" + filename;
+			user.save((err) => {
+				if (err) return console.log(err);
+				console.log("user saved manually");
+			});
+		} else {
+			const user = UserModel();
+			user.username = fields.username;
+			user.picture = fields.url;
+			user.save((err) => {
+				if (err) return console.log(err);
+				console.log("user saved with link");
+			});
+		}
+	});
+	res.status(200);
+});
+app.post("/message/create", (req, res) => {
+	const form = formidable({
+		uploadDir: __dirname + "/public", // don't forget the __dirname here
+		keepExtensions: true,
+	});
+	form.parse(req, (err, fields, files) => {
+		if (err) console.log(err);
+		console.log(fields);
+		console.log(files);
+		if (files.file) {
+			const filename = files.file.path.split("/").pop();
+			const message = MessageModel();
+			message.data.username = fields.username;
+			let unfiltered = fields.message;
+			message.data.message = unfiltered.replace(/\n/g, " "); //remove replace if you want the message to be saved in raw format
+			message.data.time = fields.time;
+			let location = "http://localhost:3000/static/" + filename;
+			console.log("location:" + location);
+			message.data.image = {
+				url: location,
+			};
+			message.save((err) => {
+				if (err) return console.log(err);
+				console.log("message saved with file uploaded");
+			});
+		} else {
+			const message = MessageModel();
+			message.data.username = fields.username;
+			let unfiltered = fields.message;
+			message.data.message = unfiltered.replace(/\n/g, " "); //remove replace if you want the message to be saved in raw format
+			message.data.time = fields.time;
+			message.data.image = { url: fields.url };
+			message.save((err) => {
+				if (err) return console.log(err);
+				console.log("message saved");
+			});
+		}
+	});
+});
+
+app.post("/user/get", (req, res) => {
+	const userToFind = req.body.username;
+	UserModel.findOne({ username: userToFind }, (err, user) => {
+		if (err) console.log(err);
+		console.log(userToFind);
+		if (user) {
+			res.send(user).status(409);
+		} else {
+			res.sendStatus(200);
+		}
+	});
+});
+
+app.get("/usercount", (req, res) => {
+	UserModel.countDocuments({}, function (err, count) {
+		res.json({ users: count });
+	});
+});
+app.get("/messagecount", (req, res) => {
+	MessageModel.countDocuments({}, function (err, count) {
+		res.json({ message: count });
+	});
+});
+
+app.get("/messagelist", function (req, res) {
+	console.log("message fired");
+	MessageModel.find({}, function (err, messages) {
+		res.send({ messages: messages });
 	});
 });
 
