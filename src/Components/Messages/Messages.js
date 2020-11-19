8fad0635d71919dba12a1f65284817ce889952dc
c1623793 <c1623793CULM@gmail.com>
2020-11-19 16:38:00 +0800

    feat:persist messages

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,17 +1,37 @@
 import React, { useRef, useEffect, useState } from "react";
-import { io, socket } from "../../Service/Socket";
+import { socket } from "../../Service/Socket";
 import style from "./Messages.module.scss";
+import axios from "axios";
 
+const API_MESSAGES_ENDPOINT = "http://localhost:3000/messagelist";
 const Messages = ({ username }) => {
 	const socketRef = useRef();
 	const [messages, setMessages] = useState([]);
+	const [retreivedMessage, setRetrievedMessage] = useState([]);
+	const [isShown, setShown] = useState(false);
+
 	useEffect(() => {
 		socketRef.current = socket;
 		socketRef.current.on("message", (data) => {
 			setMessages((messages) => [...messages, data]);
 		});
+		try {
+			const retrieveData = async () => {
+				const res = await axios.get(API_MESSAGES_ENDPOINT);
+				const incoming = await res.data.messages;
+				setRetrievedMessage(incoming);
+			};
+			retrieveData();
+		} catch (err) {
+			console.log(err);
+		}
 	}, []);
-
+	const handleMouseHover = () => {
+		setShown(true);
+	};
+	const handleMouseOut = () => {
+		setShown(false);
+	};
 	return (
 		<div className={style.MainDiv}>
 			<h3 className="font-weight-bold">Chat Messages</h3>
@@ -19,25 +39,64 @@ const Messages = ({ username }) => {
 				id="messagetext"
 				className={`mt-3 ${style.messagesContainer} container-fluid`}
 			>
-				{messages.map((message) => {
+				{retreivedMessage.map((message) => {
 					return (
-						<>
-							<p
-								key={Math.random()}
-								className={
-									message.data.username === username
-										? style.messageOwner
-										: style.messageReceiver
-								}
-							>
+						<div
+							onMouseEnter={() => handleMouseHover()}
+							onMouseLeave={() => handleMouseOut()}
+							key={message.data.time}
+							className={
+								message.data.username === username
+									? style.messageOwner
+									: style.messageReceiver
+							}
+						>
+							<img
+								className={style.avatar}
+								src={message.data.image.url || message.data.image.preview}
+								alt="user avatar"
+							/>
+							<div className="ml-3">
 								{message.data.username === username
 									? "You:"
 									: message.data.username + ":"}
-								<p>
-									{message.data.message} on {message.data.time}
-								</p>
-							</p>
-						</>
+								<br />
+								<div className="d-flex ">
+									<p>{message.data.message}</p>
+									<p className="ml-5">{isShown ? message.data.time : ""}</p>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+				{messages.map((message) => {
+					return (
+						<div
+							onMouseEnter={() => handleMouseHover()}
+							onMouseLeave={() => handleMouseOut()}
+							key={message.data.time}
+							className={
+								message.data.username === username
+									? style.messageOwner
+									: style.messageReceiver
+							}
+						>
+							<img
+								className={style.avatar}
+								src={message.data.image.preview || message.data.image.url}
+								alt="user avatar"
+							/>
+							<div className="ml-3">
+								{message.data.username === username
+									? "You:"
+									: message.data.username + ":"}
+								<br />
+								<div className="d-flex ">
+									<p>{message.data.message}</p>
+									<p className="ml-5">{isShown ? message.data.time : ""}</p>
+								</div>
+							</div>
+						</div>
 					);
 				})}
 			</div>
--- src/Components/SendMessage/SendMessage.js
+++ src/Components/SendMessage/SendMessage.js
@@ -1,30 +1,54 @@
 import React, { useRef, useEffect, useState } from "react";
-import { io, socket } from "../../Service/Socket";
+import { socket } from "../../Service/Socket";
 import moment from "moment";
 import style from "./SendMessage.module.scss";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 
-const SendMessage = ({ username }) => {
+const CREATE_MESSAGE_ENDPOINT = "http://localhost:3000/message/create";
+
+const SendMessage = ({ username, image }) => {
 	const history = useHistory();
 	const [message, setMessage] = useState("");
+	const [retainImage, setRetainImage] = useState({});
 	const socketRef = useRef();
 	socketRef.current = socket;
+	useEffect(() => {
+		setRetainImage({
+			raw: image.raw,
+			preview: image.preview,
+		});
+	}, [image]);
+	const data = {
+		data: {
+			username: username,
+			message: message,
+			time: moment().format("MMMM DD,YYYY hh:mm:ss"),
+			image: image,
+		},
+	};
+	const formData = new FormData();
+	formData.append("username", username);
+	formData.append("message", message);
+	formData.append("time", data.data.time);
+	formData.append("file", retainImage.raw);
+	formData.append("url", image.url);
 	const handleSendMessage = () => {
 		if (message === "") {
 			alert("Please enter a message");
 		} else {
-			socketRef.current.emit("message", {
-				data: {
-					username: username,
-					message: message,
-					time: moment().format("MMMM DD,YYYY hh:mm:ss"),
-				},
-			});
+			const insertMessage = async () => {
+				const res = await axios.post(CREATE_MESSAGE_ENDPOINT, formData);
+				console.log(res);
+			};
+			insertMessage();
+			socketRef.current.emit("message", data);
 			setMessage("");
 		}
 	};
 	const handleMessageText = (e) => {
-		setMessage(e.target.value);
+		const message = e.target.value;
+		setMessage(message);
 	};
 	const handleExit = () => {
 		socketRef.current.disconnect();
@@ -33,7 +57,7 @@ const SendMessage = ({ username }) => {
 	};
 	const handleInputEnter = (e) => {
 		if (e.keyCode === 13) {
-			if (message === "") {
+			if (message === "" || message === " " || message === undefined) {
 				alert("Please enter a message");
 			}
 			if (!e.shiftKey) {
@@ -52,6 +76,8 @@ const SendMessage = ({ username }) => {
 					value={message}
 					required={true}
 					onKeyUp={handleInputEnter}
+					maxLength="50"
+					placeholder="Start your conversation and say hi to the group"
 				/>
 				<button
 					className={style.sendButton}
